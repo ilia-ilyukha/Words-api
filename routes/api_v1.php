771d6383eaef5827294e87fb6ced1cfc3519c621
ilia-ilyukha <ilia.ilyukha@gmail.com>
2024-08-19 23:54:26 +0300

    Forth lesson - api versions in laravel 11

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,18 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\TicketController;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(base_path('routes/api_v1.php'));
-Route::prefix('v2')->group(base_path('routes/api_v2.php'));
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/', function() {
-    return response()->json([
-        'message' => 'Hello, API!'
-    ], 200);
-});
+Route::apiResource('/tickets', TicketController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
