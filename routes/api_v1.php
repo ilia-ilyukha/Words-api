e06fc0f4eb5ae9dfc3f03622daf33d3c4b21766e
ilia-ilyukha <ilia.ilyukha@gmail.com>
2024-08-20 23:11:30 +0300

    008. Conditionally Omitting and Including Data

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\Api\V1\UsersController;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('users', UsersController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
