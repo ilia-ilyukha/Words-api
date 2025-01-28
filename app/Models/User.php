40283936b2490f632c7a4afadcf68a774996d072
ilia-ilyukha <ilia.ilyukha@gmail.com>
2025-01-28 17:37:39 +0200

    012. Sorting Data

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Http\Filters\V1\QueryFilter;
 use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -51,5 +53,9 @@ class User extends Authenticatable
 
     public function tickets(): HasMany {
         return $this->hasMany(Ticket::class);
+    }
+
+    public function scopeFilter(Builder $builder, QueryFilter $filters){
+        return $filters->apply($builder);
     }
 }
