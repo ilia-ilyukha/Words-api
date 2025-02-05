df7ce4a80213ed22c39dd8286ef6427ef2912693
ilia-ilyukha <ilia.ilyukha@gmail.com>
2025-02-05 17:00:39 +0200

    15.1

--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\ReplaceTicketRequest;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
@@ -44,7 +45,7 @@ class AuthorTicketsController extends ApiController
         try {
             $ticket = Ticket::findOrFail($ticket_id);
 
-            if($ticket->user_id === $author_id){
+            if ($ticket->user_id === $author_id) {
                 $ticket->delete();
 
                 return $this->ok('Ticket successfully delleted');
@@ -53,6 +54,35 @@ class AuthorTicketsController extends ApiController
             return $this->error('Ticket not found', 404);
         } catch (ModelNotFoundException $exception) {
             return $this->error('Ticket not found', 404);
+        }
+    }
+
+    public function replace(ReplaceTicketRequest $request, $author_id, $ticket_id)
+    {
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+
+            if ($ticket->user_id === $author_id) {
+                $model = [
+                    'title' => $request->input('data.attributes.title'),
+                    'description' => $request->input('data.attributes.description'),
+                    'status' => $request->input('data.attributes.status'),
+                    'user_id' => $request->input('data.relationships.author.data.id'),
+                ];
+    
+                $ticket->update($model);
+        
+                return new TicketResource($ticket);
+            }
+            // TODO: ticket doesn't belong to user
+            
+        } catch (ModelNotFoundException $exception) {
+            return $this->ok(
+                'Ticket not found',
+                [
+                    'error' => 'The provided ticket id does not exists'
+                ]
+            );
         }
     }
 }
